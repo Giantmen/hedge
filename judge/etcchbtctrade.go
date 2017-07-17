0acd62bbb9a5e7ecf9e61ce25335109fb322f116
aloongly <aloongly@gmail.com>
2017-07-17 14:40:48 +0800

    add checkout and fix bug

--- judge/etcchbtctrade.go
+++ judge/etcchbtctrade.go
@@ -68,9 +68,11 @@ func NewEtcChBtctrade(cfg *config.Judge, sr *store.Service) (*EtcChBtctrade, err
 
 func (j *EtcChBtctrade) Process() {
 	j.getAccount()
+	log.Infof("account btctrade cny:%f etc:%f", j.btctrade.cny, j.btctrade.etc)
+	log.Infof("account chbtc cny:%f etc:%f", j.chbtc.cny, j.chbtc.etc)
 	// TODO: 加入动态修改
-	var depth float64 = 1
-	var amount float64 = 0.1
+	var depth float64 = 3
+	var amount float64 = 1
 	var accounter = time.NewTicker(time.Second * 100)
 	defer accounter.Stop()
 	for {
@@ -100,6 +102,7 @@ func (j *EtcChBtctrade) judge(depth, amount float64) {
 				return
 			} else { //且向右开关关的
 				log.Debugf("禁止交易:%s -> %s %v", j.btctrade.name, j.chbtc.name, err)
+				log.Info("earn:", fmt.Sprintf("%0.2f", earn*amount), mypro.Btctrade, "sell:", btctrade.Buy, mypro.Chbtc, "buy:", chbtc.Sell)
 				return
 			}
 		} else if !j.right { //仓位正常 且向右开关关的
@@ -122,6 +125,7 @@ func (j *EtcChBtctrade) judge(depth, amount float64) {
 				return
 			} else { //且向左开关关的
 				log.Debugf("禁止交易:%s -> %s %v", j.chbtc.name, j.btctrade.name, err)
+				log.Info("earn:", fmt.Sprintf("%0.2f", earn*amount), mypro.Chbtc, "sell:", chbtc.Buy, mypro.Btctrade, "buy:", btctrade.Sell)
 				return
 			}
 		} else if !j.left { //仓位正常 且向左开关关的
@@ -130,7 +134,7 @@ func (j *EtcChBtctrade) judge(depth, amount float64) {
 		}
 
 		err := j.hedging(j.chbtc, j.btctrade, chbtc, btctrade, fmt.Sprintf("%f", amount))
-		if err != nil {
+		if err == nil {
 			log.Info("earn:", fmt.Sprintf("%0.2f", earn*amount), mypro.Chbtc, "sell:", chbtc.Buy, mypro.Btctrade, "buy:", btctrade.Sell)
 		} else {
 			log.Error("hedging err", err)
@@ -142,9 +146,9 @@ func (j *EtcChBtctrade) judge(depth, amount float64) {
 func (j *EtcChBtctrade) checkAccount(sellSide, buySide account, priceS, pirceB *proto.Price, amount string) error {
 	num, _ := strconv.ParseFloat(amount, 64)
 	if sellSide.etc < num {
-		return fmt.Errorf("%s etc 余额不足:%f cny:%f", sellSide.name, sellSide.etc, sellSide.cny)
+		return fmt.Errorf("%s:etc余额不足:%f cny:%f", sellSide.name, sellSide.etc, sellSide.cny)
 	} else if buySide.cny < (pirceB.Sell * num) {
-		return fmt.Errorf("%s cny 余额不足:%f etc:%f", buySide.name, buySide.cny, buySide.etc)
+		return fmt.Errorf("%s:cny余额不足:%f etc:%f", buySide.name, buySide.cny, buySide.etc)
 	}
 	return nil
 }
@@ -165,16 +169,16 @@ func (j *EtcChBtctrade) hedging(sellSide, buySide account, priceS, pirceB *proto
 	order, err = j.deal(buySide.bourse, proto.BUY, amount, fmt.Sprintf("%0.3f", pirceB.Sell))
 	if err != nil {
 		log.Error(err)
-		if order, err = j.retry(buySide.bourse, proto.BUY, amount); err != nil {
+		if order, err = j.retryDeal(buySide.bourse, proto.BUY, amount); err != nil {
 			return err //重试失败
 		}
-		log.Info("retry ok")
+		log.Info(sellSide.name, "buy retry ok")
 	}
 	log.Info("buy:", pirceB.Sell, amount, order, order.OrderID, order.Status)
 	return nil
 }
 
-func (j *EtcChBtctrade) retry(bou bourse.Bourse, side, amount string) (*proto.Order, error) {
+func (j *EtcChBtctrade) retryDeal(bou bourse.Bourse, side, amount string) (*proto.Order, error) {
 	sec := rand.Intn(10)
 	for {
 		time.Sleep(time.Duration(sec) * time.Millisecond)
@@ -182,7 +186,7 @@ func (j *EtcChBtctrade) retry(bou bourse.Bourse, side, amount string) (*proto.Or
 		if order, err := j.deal(bou, side, amount, fmt.Sprintf("%0.3f", price.Sell)); err == nil {
 			return order, err
 		} else {
-			log.Error("retry err", err)
+			log.Errorf("retry err %v", err)
 		}
 		sec = sec << 1
 		if sec > 100 {
@@ -207,7 +211,48 @@ func (j *EtcChBtctrade) deal(bou bourse.Bourse, side, amount, price string) (*pr
 			return nil, err
 		}
 	}
-	return bou.GetOneOrder(order.OrderID, order.Currency)
+	return j.checkOrder(bou, order.OrderID, order.Currency)
+}
+
+func (j *EtcChBtctrade) checkOrder(bou bourse.Bourse, orderId, currencyPair string) (*proto.Order, error) {
+	sec := 5
+	for {
+		if order, err := bou.GetOneOrder(orderId, currencyPair); err != nil || order.Status == proto.ORDER_UNFINISH {
+			log.Error("retry ordercheck", order.Status, err)
+		} else {
+			return order, err
+		}
+		time.Sleep(time.Duration(sec) * time.Millisecond)
+		sec = sec << 1
+		if sec > 80 {
+			if err := j.cancelOrder(bou, orderId, currencyPair); err != nil {
+				log.Error(err)
+				return nil, fmt.Errorf("%s retry ordercheck cancel err", orderId)
+			}
+			return nil, fmt.Errorf("%s retry ordercheck err cancel ok", orderId)
+		}
+	}
+}
+
+func (j *EtcChBtctrade) cancelOrder(bou bourse.Bourse, orderId, currencyPair string) error {
+	sec := rand.Intn(5)
+	for {
+		if cancel, err := bou.CancelOrder(orderId, currencyPair); !cancel || err != nil {
+			if order, err := bou.GetOneOrder(orderId, currencyPair); err != nil || order.Status == proto.ORDER_UNFINISH {
+				log.Error("CancelOrder GetOneOrder err", order.Status, err)
+			} else {
+				return nil
+			}
+			log.Error("retry cancelOrder", cancel, err)
+		} else {
+			return nil
+		}
+		time.Sleep(time.Duration(sec) * time.Millisecond)
+		sec = sec << 1
+		if sec > 50 {
+			return fmt.Errorf("retry cancelOrder err")
+		}
+	}
 }
 
 func (j *EtcChBtctrade) getAccount() {
